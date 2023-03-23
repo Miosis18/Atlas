5ea6cac23fb33b626ba258bcc0a3f1862c6d0c3f
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-23 02:53:17 +0000

    Added planned general commands and basic structure

--- cogs/commands/general/dog.py
+++ cogs/commands/general/dog.py
@@ -1,7 +1,6 @@
 import discord
 import os
 import yaml
-import requests
 from discord import app_commands
 from discord.ext import commands
 
@@ -11,7 +10,7 @@ with open("./configs/config.yml") as config:
 GUILD_ID = int(CONFIG["GuildID"])
 
 
-class Advice(commands.Cog):
+class Dog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.Bot = bot
 
@@ -21,17 +20,14 @@ class Advice(commands.Cog):
     async def on_ready(self):
         print(f"[COMMAND] {os.path.basename(__file__)} loaded.")
 
-    # Advice command
+    # Dog command
 
-    @app_commands.command(name="advice", description="Get random advice")
+    @app_commands.command(name="dog", description="Get random advice")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
-    async def advice(self, interaction: discord.Interaction) -> None:
+    async def dog(self, interaction: discord.Interaction) -> None:
 
-        request = requests.get("http://api.adviceslip.com/advice").json()
-        advice_message = request["slip"]["advice"]
-
-        await interaction.response.send_message(advice_message)
+        pass
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Advice(bot))
+    await bot.add_cog(Dog(bot))
