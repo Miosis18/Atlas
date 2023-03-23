7bdab454ca6adb6ae8be16083ff29fca7b8b7ab9
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-23 05:55:12 +0000

    Created the darkjoke command.

--- cogs/commands/general/darkjoke.py
+++ cogs/commands/general/darkjoke.py
@@ -1,6 +1,9 @@
 import discord
 import os
 import yaml
+import json
+import random
+import datetime as dt
 from discord import app_commands
 from discord.ext import commands
 
@@ -26,7 +29,17 @@ class DarkJoke(commands.Cog):
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def dark_joke(self, interaction: discord.Interaction) -> None:
 
-        pass
+        with open("./data/json/dark_jokes.json", "r", encoding="UTF-8") as file:
+            data = json.load(file)
+
+        dark_joke_embed = discord.Embed(title="**DARK JOKE** :rofl:",
+                                        description=random.choice(data["DarkJokes"]),
+                                        color=int(CONFIG["EmbedColors"].replace("#", ""), 16),
+                                        timestamp=dt.datetime.now())
+        dark_joke_embed.set_footer(text=f"{CONFIG['BotName']} - Command issued by {interaction.user.name}#"
+                                        f"{interaction.user.discriminator}", icon_url=self.Bot.user.display_avatar)
+
+        await interaction.response.send_message(embed=dark_joke_embed)
 
 
 async def setup(bot: commands.Bot) -> None:
