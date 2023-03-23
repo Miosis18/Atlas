a75c96f5ac2f6c3035eaaf5d4a8eb7d1a990572e
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-23 06:38:45 +0000

    Completed the fliptext command.

--- cogs/commands/general/fliptext.py
+++ cogs/commands/general/fliptext.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import yaml
+import upsidedown
+import datetime as dt
 from discord import app_commands
 from discord.ext import commands
 
@@ -24,9 +26,15 @@ class FlipText(commands.Cog):
 
     @app_commands.command(name="fliptext", description="Get random advice")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
-    async def flip_text(self, interaction: discord.Interaction) -> None:
+    async def flip_text(self, interaction: discord.Interaction, text: str) -> None:
 
-        pass
+        flipped_text_embed = discord.Embed(description=upsidedown.transform(text),
+                                           color=int(CONFIG["EmbedColors"].replace("#", ""), 16),
+                                           timestamp=dt.datetime.now())
+        flipped_text_embed.set_footer(text=f"{CONFIG['BotName']} - Command issued by {interaction.user.name}#"
+                                           f"{interaction.user.discriminator}", icon_url=self.Bot.user.display_avatar)
+
+        await interaction.response.send_message(embed=flipped_text_embed)
 
 
 async def setup(bot: commands.Bot) -> None:
