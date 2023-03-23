200b94d8d7a2885cb55ae0e82581bcc86dc44c11
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-23 06:31:41 +0000

    Completed the dogfact command.

--- cogs/commands/general/dogfact.py
+++ cogs/commands/general/dogfact.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import yaml
+import requests
+import datetime as dt
 from discord import app_commands
 from discord.ext import commands
 
@@ -26,7 +28,16 @@ class DogFact(commands.Cog):
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def dog_fact(self, interaction: discord.Interaction) -> None:
 
-        pass
+        request = requests.get(url="https://dog-api.kinduff.com/api/facts").json()
+
+        dog_fact_embed = discord.Embed(title=f"**Facts about dogs** :dog:",
+                                       description=request["facts"][0],
+                                       color=int(CONFIG["EmbedColors"].replace("#", ""), 16),
+                                       timestamp=dt.datetime.now())
+        dog_fact_embed.set_footer(text=f"{CONFIG['BotName']} - Command issued by {interaction.user.name}#"
+                                       f"{interaction.user.discriminator}", icon_url=self.Bot.user.display_avatar)
+
+        await interaction.response.send_message(embed=dog_fact_embed)
 
 
 async def setup(bot: commands.Bot) -> None:
