0f27f6b96d062f3e11e37b259b838aa1ed359353
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-23 06:09:30 +0000

    Created the dog command with checks to ensure jpg.

--- cogs/commands/general/dog.py
+++ cogs/commands/general/dog.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import yaml
+import requests
+import datetime as dt
 from discord import app_commands
 from discord.ext import commands
 
@@ -26,7 +28,23 @@ class Dog(commands.Cog):
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def dog(self, interaction: discord.Interaction) -> None:
 
-        pass
+        appropriate_image = False
+        image = "https://i.pinimg.com/564x/7e/c3/5a/7ec35aa21a3212ed7658f7ff9542c9db.jpg"
+
+        while appropriate_image is False:
+            request = requests.get(url="https://random.dog/woof.json").json()
+            if request["url"][-3:] == "jpg":
+                image = request["url"]
+                appropriate_image = True
+
+        dog_embed = discord.Embed(title="**Woof Woof** :dog:",
+                                  color=int(CONFIG["EmbedColors"].replace("#", ""), 16),
+                                  timestamp=dt.datetime.now())
+        dog_embed.set_image(url=image)
+        dog_embed.set_footer(text=f"{CONFIG['BotName']} - Command issued by {interaction.user.name}#"
+                                  f"{interaction.user.discriminator}", icon_url=self.Bot.user.display_avatar)
+
+        await interaction.response.send_message(embed=dog_embed)
 
 
 async def setup(bot: commands.Bot) -> None:
