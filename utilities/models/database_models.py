c6e6bc1f7b3a601913dabcdb6fb3ff6719d70368
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-03-26 20:50:56 +0100

    Created suggestion and member db models

--- utilities/models/database_models.py
+++ utilities/models/database_models.py
@@ -5,4 +5,29 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-# Add Models Here
+class Members(Base):
+    __tablename__ = "members"
+
+    member_id = Column(Integer, primary_key=True)
+    user_id = Column(String(100), nullable=False)
+    username = Column(String(100), nullable=False)
+    date_joined = Column(String(100), nullable=False)
+
+    def get_id(self):
+        return self.member_id
+
+
+class Suggestions(Base):
+    __tablename__ = "suggestions"
+
+    suggestion_id = Column(Integer, primary_key=True)
+    message_id = Column(String(100), nullable=False)
+    author_id = Column(String(100), nullable=False)
+    content = Column(String(1000), nullable=False)
+    up_votes = Column(Integer, nullable=False)
+    down_votes = Column(Integer, nullable=False)
+    status = Column(String(25), nullable=False)
+
+    def get_id(self):
+        return self.suggestion_id
+
