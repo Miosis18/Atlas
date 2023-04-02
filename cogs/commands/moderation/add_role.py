a7c205ea5208405deb1d002238ad9fda708f1fe4
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-04-02 19:13:30 +0100

    Completed add_role.py

--- cogs/commands/moderation/add_role.py
+++ cogs/commands/moderation/add_role.py
@@ -24,9 +24,25 @@ class AddRole(commands.Cog):
 
     @app_commands.command(name="addrole", description="Get random advice")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
-    async def add_role(self, interaction: discord.Interaction) -> None:
+    @app_commands.default_permissions(manage_roles=True)
+    async def add_role(self, interaction: discord.Interaction, user: discord.Member, role: discord.Role) -> None:
 
-        pass
+        if role not in user.roles:
+            if interaction.user.id != user.id:
+                if interaction.user.roles[-1].position >= role.position:
+                    try:
+                        await user.add_roles(role)
+                        await interaction.response.send_message("Role added")
+                    except discord.errors.Forbidden:
+                        await interaction.response.send_message("I cannot add this role as I do not have a role "
+                                                                "high enough to do this.", ephemeral=True)
+                else:
+                    await interaction.response.send_message("You cannot do this as you do not have permission to "
+                                                            "promote someone this high", ephemeral=True)
+            else:
+                await interaction.response.send_message("You cannot add roles to yourself", ephemeral=True)
+        else:
+            await interaction.response.send_message("They already have the role", ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
