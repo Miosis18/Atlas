437300cadcc858b8932ff289b4630e2b9ce59599
Elliot Ramsden <ElliotARamsden@Outlook.Com>
2023-04-03 19:18:06 +0100

    Completed poll.py

--- cogs/commands/moderation/poll.py
+++ cogs/commands/moderation/poll.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import yaml
+import datetime as dt
 from discord import app_commands
 from discord.ext import commands
 
@@ -20,13 +21,23 @@ class Poll(commands.Cog):
     async def on_ready(self):
         print(f"[COMMAND] {os.path.basename(__file__)} loaded.")
 
-    # Suggest command
+    # Poll command
 
     @app_commands.command(name="poll", description="Get random advice")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
-    async def poll(self, interaction: discord.Interaction) -> None:
+    async def poll(self, interaction: discord.Interaction, question: str) -> None:
 
-        pass
+        poll_embed = discord.Embed(title="A poll has been started!",
+                                   description=question,
+                                   color=int(CONFIG["EmbedColors"].replace("#", ""), 16),
+                                   timestamp=dt.datetime.utcnow())
+        poll_embed.set_footer(text=f"Poll started by: {interaction.user.name}#{interaction.user.discriminator}",
+                              icon_url=interaction.user.display_avatar.url)
+
+        await interaction.response.send_message(embed=poll_embed)
+        new_poll_message = await interaction.original_response()
+        await new_poll_message.add_reaction("\U0001F44D")
+        await new_poll_message.add_reaction("\U0001F44E")
 
 
 async def setup(bot: commands.Bot) -> None:
